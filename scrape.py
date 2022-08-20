777ad6a9b7e1ca8d3cce1ba70fe972b2c2beb43b
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2022-08-20 13:23:44 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,7 +4,22 @@ from csv import writer
 
 state=input("Enter the state: ")
 district=input("Enter the district: ")
-
+if(state=="ARUNACHAL PRADESH"):
+    state=ARUNACHAL+PRADESH
+elif(state=='ANDHRA PRADESH'):
+    state='ANDHRA+PRADESH'
+elif(state=='DAMAN & DIU'):
+    state='DAMAN+%26+DIU'
+elif(state=='HIMACHAL PRADESH'):
+    state='HIMACHAL+PRADESH'
+elif(state=='JAMMU & KASHMIR'):
+    state='JAMMU+%26+KASHMIR'
+elif(state=='MADHYA PRADESH'):
+    state='MADHYA+PRADESH'
+elif(state=='UTTAR PRADESH'):
+    state='UTTAR+PRADESH'
+elif(state=='WEST BENGAL'):
+    state='WEST+BENGAL'
 
 
 html_text=requests.get("https://nmba.dosje.gov.in/photo-gallery-dashboard.php?state="+state+"&district="+district+"&filter=#").text
@@ -15,6 +30,5 @@ lists=soup.find_all('div',class_='h600')
 fh=open('data.txt','w',encoding="utf-8")
 
 for l in lists:
-    # fh.write(l.text)
     s=l.text
     fh.write(s)
